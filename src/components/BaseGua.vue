85b0a5cc1509f43cf4c51e1457ca488abd2d59f5
hengxiao <hengxiao@iflytek.com>
2023-07-10 00:56:01 +0800

    ✨ feat(display): 六爻简化

--- src/components/BaseGua.vue
+++ src/components/BaseGua.vue
@@ -10,25 +10,38 @@ const props = withDefaults(defineProps<{
   gan?: DATABASE.Tiangan_Key
   zhi?: DATABASE.Dizhi_Key
   yongshen?: string
+  hasCenter?: boolean
   hasLiushen?: boolean
   hasNajia?: boolean
   hasDongyao?: boolean
+  hasShiying?: boolean
   bagongWuxing?: string
   guaxiang: number[]
+  // fontSize: string
 }>(), {
   size: 40,
   gan: '甲',
   zhi: '子',
   yongshen: '',
   hasLiushen: false,
-  hasNajia: false,
+  hasNajia: true,
   hasDongyao: true,
+  hasShiying: false,
+  // fontSize: '14px',
   // bagongWuxing: '金',
   guaxiang: () => [6, 7, 8, 9, 8, 7],
 })
 
 // emit
 const emit = defineEmits(['on-change'])
+
+const 六亲_简化 = {
+  父母: '父',
+  兄弟: '兄',
+  子孙: '孙',
+  妻财: '财',
+  官鬼: '鬼',
+} as DATABASE.Guali_EN
 
 // data
 const { zhouyi } = useZhouyiStore()
@@ -45,6 +58,9 @@ const 用神_五行 = computed(() => 用神.value.slice(-1)) as ComputedRef<DATA
 // const 用神_地支 = computed(() => 用神.value.slice(-2, -1)) as ComputedRef<DATABASE.Dizhi_Key>
 const 八宫五行 = computed(() => props.bagongWuxing || 卦.value.五行) as ComputedRef<DATABASE.Wuxing_Key>
 
+const 卦宫_卦 = computed(() => yijing.find((v) => v.卦名 === 卦.value?.八宫.slice(0, 1)) || 卦.value)
+const 卦宫_纳甲 = computed(() => 卦宫_卦.value?.纳甲)
+
 // method
 const getLiuqin = (wuxing: DATABASE.Wuxing, wuxingStr: string) => {
   const wuxingKey = wuxingStr as DATABASE.Wuxing_Key
@@ -55,12 +71,17 @@ const getYinyuan = (五行: DATABASE.Wuxing_Key, 地支: string) => yinyuan[五�
 // style
 const width = computed(() => `${props.size}vw`)
 const height = computed(() => `${props.size / 6}vw`)
-const fontSize = computed(() => props.size <= 24 ? `${props.size / 2}px` : '14px')
+const fontSize = computed(() => `${props.size / 2}px`)
+
+// mounted
+onMounted(() => {
+  props.hasDongyao && emit('on-change', 纳甲.value[0])
+})
 </script>
 
 <template>
   <div class="BaseGua flex flex-col-reverse">
-    <div v-for="(yao, index) in guaxiang" :key="index" class="item flex-center" @click="emit('on-change', 纳甲[index])">
+    <div v-for="(yao, index) in guaxiang" :key="index" class="item flex items-center" @click="emit('on-change', 纳甲[index])">
       <div v-if="hasNajia" class="BaseGua-left">
         <div v-if="index === 5">
           <p opacity-0>
@@ -76,16 +97,26 @@ const fontSize = computed(() => props.size <= 24 ? `${props.size / 2}px` : '14px
             'border-base': dizhi[zhi].六冲 === 纳甲[index].slice(-2, -1),
           }"
         >
-          <p v-if="hasLiushen" class="mr-2">
+          <!--
+            <p v-if="hasLiushen" class="mr-2">
             {{ 六神[index] }}
-          </p>
+            </p>
+          -->
+          <template v-if="hasDongyao">
+            <p color-gray-4>
+              {{ 六亲_简化[getLiuqin(wuxing, 卦宫_纳甲[index].slice(-1))] }}
+            </p>
+            <p color-gray-4 mr-2>
+              {{ 卦宫_纳甲[index].slice(-2, -1) }}
+            </p>
+          </template>
           <p>{{ getLiuqin(wuxing, 纳甲[index].slice(-1)) }}{{ 纳甲[index].slice(-2) }}</p>
           <p class="mr-2">
             {{ getYinyuan(用神_五行, 纳甲[index].slice(-2, -1)).slice(-1) }}
           </p>
         </div>
       </div>
-      <div class="BaseGua-center">
+      <div v-if="hasCenter" class="BaseGua-center">
         <div v-if="index === 5" flex-center>
           <p class="font-bold">
             {{ 卦?.卦名 }}
@@ -94,22 +125,42 @@ const fontSize = computed(() => props.size <= 24 ? `${props.size / 2}px` : '14px
         </div>
         <div class="BaseGua-yao m-1" :class="[yao % 2 === 0 ? 'yin' : 'yang']"></div>
       </div>
-      <div v-if="hasDongyao" class="BaseGua-right">
+      <div class="BaseGua-right text-left">
         <div v-if="index === 5">
           <p opacity-0>
             <!-- 占位符 -->
             占
           </p>
         </div>
-        <div flex w-6>
-          <p w-3>
-            {{ yao === 6 ? 'x' : yao === 9 ? 'o' : '' }}
+        <div flex>
+          <p w-8 text-xl color-gray-6>
+            <span
+              v-if="hasDongyao" inline-block :class="{
+                'color-black': yao % 3 === 0,
+                'translate-y--4px': yao % 3 !== 0,
+                'translate-y--2px': yao === 9,
+              }"
+            >
+              {{ yao === 6 ? '×' : yao === 9 ? 'o' : yao === 7 ? '、' : '、、' }}
+            </span>
+            <span v-else inline-block translate-y--4px>
+              {{ yao % 2 === 0 ? '、' : '、、' }}
+            </span>
           </p>
-          <p v-if="hasNajia">
-            {{ 世应[`${index + 1}`] }}
+          <p v-if="hasShiying" translate-y-4px>
+            {{ 世应[`${index + 1}`] === '世' ? 卦?.八宫.slice(0, 1) : 世应[`${index + 1}`] }}
+          </p>
+          <p v-if="!hasDongyao" ml-2 translate-y-4px>
+            {{ 六神[index] }}
           </p>
         </div>
       </div>
+    </div>
+    <div v-if="!hasCenter" flex-center translate-y-10px>
+      <p class="font-bold">
+        {{ 卦?.卦名 }}
+      </p>
+      <p>({{ 卦?.八宫.slice(0, 2) }}{{ 卦?.归游 }})</p>
     </div>
   </div>
 </template>
