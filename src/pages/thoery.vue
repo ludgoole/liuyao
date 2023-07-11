25e4883a90fa7697419edb50283caa4f2da3fa23
hengxiao <hengxiao@iflytek.com>
2023-07-11 19:41:37 +0800

    ✨ feat(thoery): 增加理论

---
+++ src/pages/thoery.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import img_dizhi from '@/assets/image/dizhi.webp'
+import img_zixing from '@/assets/image/zixing.webp'
+import img_wuxing from '@/assets/image/wuxing.png'
+</script>
+
+<template>
+  <div>
+    <img :src="img_wuxing" />
+    <img :src="img_dizhi" />
+    <img :src="img_zixing" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
