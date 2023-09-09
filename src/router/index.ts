6bea999b3988581d001c50f7867d4a243412b710
hengxiao <hengxiao@iflytek.com>
2023-09-09 13:35:36 +0800

    🎈 perf(login): 取消登录拦截

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import localforage from 'localforage'
-import { Toast } from 'vant'
+// import localforage from 'localforage'
+// import { Toast } from 'vant'
 import routes from '~pages'
 
 export const router = createRouter({
@@ -8,24 +8,24 @@ export const router = createRouter({
   routes,
 })
 
-router.beforeEach(async (to, from, next) => {
-  const token = await localforage.getItem('Authorization')
-  const user = (await localforage.getItem('userInfo') || {}) as { role: string }
-  const roles = to.meta?.roles as string[]
-  const isLoginPage = to.path === '/login'
-  const hasToken = !!token
-  const needRoles = !!roles
-  const hasRole = roles && roles.includes(user.role)
+// router.beforeEach(async (to, from, next) => {
+//   const token = await localforage.getItem('Authorization')
+//   const user = (await localforage.getItem('userInfo') || {}) as { role: string }
+//   const roles = to.meta?.roles as string[]
+//   const isLoginPage = to.path === '/login'
+//   const hasToken = !!token
+//   const needRoles = !!roles
+//   const hasRole = roles && roles.includes(user.role)
 
-  if (!isLoginPage && !hasToken) {
-    Toast({ message: '您还没有登录，请先登录' })
-    next('/login')
-  }
-  else if (!isLoginPage && hasToken && needRoles && !hasRole) {
-    Toast({ message: '您没有权利访问，请联系管理员升级' })
-    next('/404')
-  }
-  else {
-    next()
-  }
-})
+//   if (!isLoginPage && !hasToken) {
+//     Toast({ message: '您还没有登录，请先登录' })
+//     next('/login')
+//   }
+//   else if (!isLoginPage && hasToken && needRoles && !hasRole) {
+//     Toast({ message: '您没有权利访问，请联系管理员升级' })
+//     next('/404')
+//   }
+//   else {
+//     next()
+//   }
+// })
